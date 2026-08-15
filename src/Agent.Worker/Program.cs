245da94b5e1c0e8ff998ec88cbb9fe34042713b1
Matt Cavallo <macavall@microsoft.com>
2026-08-15 07:54:27 -0500

    fix

--- src/Agent.Worker/Program.cs
+++ src/Agent.Worker/Program.cs
@@ -7,12 +7,14 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure<FileBusOptions>(builder.Configuration.GetSection("FileBus"));
 builder.Services.Configure<WorkerOptions>(builder.Configuration.GetSection("Worker"));
 builder.Services.Configure<KustoOptions>(builder.Configuration.GetSection("Kusto"));
+builder.Services.Configure<ShellOptions>(builder.Configuration.GetSection("Shell"));
 
 builder.Services.AddSingleton(sp =>
     new FileBus(sp.GetRequiredService<IOptions<FileBusOptions>>().Value));
 
 builder.Services.AddSingleton<ITaskHandler, EchoTaskHandler>();
 builder.Services.AddSingleton<ITaskHandler, KustoTaskHandler>();
+builder.Services.AddSingleton<ITaskHandler, ShellTaskHandler>();
 builder.Services.AddHostedService<WatcherWorker>();
 
 var host = builder.Build();
